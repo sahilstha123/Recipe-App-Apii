ab74c7dd5429f0a04ecc5e337a86a14e6e09603f
sahilstha123 <sthasahil401@gmail.com>
2025-04-28 13:42:02 +0545

    creating own custom model

--- app/core/models.py
+++ app/core/models.py
@@ -1,2 +1,28 @@
-from django.db import models # noqa
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,  # Base class for creating custom user models
+    PermissionsMixin,  # Adds permission-related fields and methods
+    BaseUserManager    # A helper class for creating custom user managers
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
+
+    # This tells Django to use the email as the unique field for
+    #  authentication instead of the default "username".
+    USERNAME_FIELD = "email"
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,6 +5,7 @@ Test for models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test models."""
     def test_create_user_with_email_successful(self):
@@ -15,5 +16,5 @@ class ModelTests(TestCase):
             email=email,
             password=password
         )
-        self.assertEqual(user.email,email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
