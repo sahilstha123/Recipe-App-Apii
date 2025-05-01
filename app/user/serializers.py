e44e1c07e749156d8f7891948d0a7888bb603d58
sahilstha123 <sthasahil401@gmail.com>
2025-05-01 15:43:44 +0545

    creating user api finish

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,9 @@
 """
 serializer for the user API
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (get_user_model, authenticate)
+
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 
@@ -15,3 +17,40 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validate_data):
         """ Create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validate_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user token"""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
+
+    def update(self, instance, vaildate_data):
+        """updating the existing user"""
+        password = vaildate_data.pop('password', None)
+        user = super().update(instance, vaildate_data)
+
+        if password:
+            user.set_password(password)
+            user.save
+        return user
